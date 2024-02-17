2b5d1789d595c963c28ff77bdf1a9cd99f2683ff
davdav79 <d.eichhorn1@gmx.net>
2024-02-17 08:12:43 +0100

    led strip wird richtig angesteuert, end frame wird noch nicht korrekt generiert

--- LED-Strip/src/TestLEDStrip.cpp
+++ LED-Strip/src/TestLEDStrip.cpp
@@ -11,8 +11,7 @@ extern "C"
 {
   // function prototypes debug
   void TOGGLE_LED(void);
-  void SS_ON(void);
-  void SS_OFF(void);
+  void LED_init(void);
   // function prototypes
   void STRIP_SPI_init(void);
   void STRIP_com(uint8_t one_byte);
@@ -22,40 +21,53 @@ extern "C"
 
 void setup()
 {
+  LED_init();
   STRIP_SPI_init();
 }
-void loop()
-{
-  TOGGLE_LED();
-  // LED-ON
-  // Start-Frame
-  STRIP_com(0);
-  STRIP_com(0);
-  STRIP_com(0);
-  STRIP_com(0);
-  // LED-Frame
-  STRIP_com(0xE5); // Brigtness 5
-  STRIP_com(0x0); // Blue
-  STRIP_com(0);    // Green
-  STRIP_com(0);    // Red
-  // END-Frame
-  STRIP_com(0);
-  delay(500);
-  // LED-OFF
-  // Start-Frame
-  STRIP_com(0);
-  STRIP_com(0);
-  STRIP_com(0);
-  STRIP_com(0);
-  // LED-Frame
-  STRIP_com(0xE0); // Brigtness 0
-  STRIP_com(0);    // Blue
-  STRIP_com(0);    // Green
-  STRIP_com(0);    // Red
-  // END-Frame
-  STRIP_com(0);
-  delay(500);
-}
+// void loop()
+// {
+//   TOGGLE_LED();
+//   // LED-ON
+//   // Start-Frame
+//   STRIP_com(0);
+//   STRIP_com(0);
+//   STRIP_com(0);
+//   STRIP_com(0);
+//   // LED-Frame
+//   STRIP_com(0xE5); // Brigtness 5
+//   STRIP_com(0x0A); // Blue
+//   STRIP_com(0);    // Green
+//   STRIP_com(0);    // Red
+//   // END-Frame
+//   STRIP_com(0);
+//   delay(500);
+//   // LED-OFF
+//   // Start-Frame
+//   STRIP_com(0);
+//   STRIP_com(0);
+//   STRIP_com(0);
+//   STRIP_com(0);
+//   // LED-Frame
+//   STRIP_com(0xE5); // Brigtness 0
+//   STRIP_com(0);    // Blue
+//   STRIP_com(0x0A);    // Green
+//   STRIP_com(0);    // Red
+//   // END-Frame
+//   STRIP_com(0);
+// delay(500);
+//     STRIP_com(0);
+//   STRIP_com(0);
+//   STRIP_com(0);
+//   STRIP_com(0);
+//   // LED-Frame
+//   STRIP_com(0xE5); // Brigtness 0
+//   STRIP_com(0);    // Blue
+//   STRIP_com(0x0);    // Green
+//   STRIP_com(0xA);    // Red
+//   // END-Frame
+//   STRIP_com(0);
+//   delay(500);
+// }
 
 // void loop()
 // {
@@ -68,18 +80,18 @@ void loop()
 //   delay(500);
 // }
 
-// void loop()
-// {
-//     END_frame(0); // 1 x End-Frame
-//     delay(200);
-//     END_frame(16); // 1 x End-Frame
-//     delay(200);
-//     END_frame(17); // 2 x End-Frame
-//     delay(200);
-//     END_frame(33); // 3 x End-Frame
-//     delay(200);
-//     END_frame(241); // 16 x End-Frame
-//     delay(200);
-//     END_frame(256); // 16 x End-Frame
-//     delay(200);
-// }
+void loop()
+{
+    END_frame(0); // 1 x End-Frame
+    delay(200);
+    END_frame(16); // 1 x End-Frame
+    delay(200);
+    END_frame(17); // 2 x End-Frame
+    delay(200);
+    END_frame(33); // 3 x End-Frame
+    delay(200);
+    END_frame(241); // 16 x End-Frame
+    delay(200);
+    END_frame(256); // 16 x End-Frame
+    delay(200);
+}
