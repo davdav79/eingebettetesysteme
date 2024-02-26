e220affd34a0418b8780f9964a32dd0e90fdd4b3
scheggaB <bastizahn@googlemail.com>
2024-02-26 14:55:50 +0100

    Aufgabe 3 ausführlich kommenitert Logik ist gleichgeblieben. Funktionen getestet und sind korrekt.

--- LED-Strip/src/TestLEDStrip.cpp
+++ LED-Strip/src/TestLEDStrip.cpp
@@ -7,6 +7,13 @@
 
 #define LED_NUM_MAX 30
 
+// The extern "C" block is used to tell the compiler that the functions inside it are C functions, not C++ functions. This is important because C++ mangles function names, which means that the linker can't find the functions in the C library. By using extern "C", you tell the compiler not to mangle the function names, so that the linker can find them.
+// Strip.S implements all these functions and with .global it makes them available to the linker. The linker then links the C++ code with the assembly code.
+// platformio builds the C++ code and the assembly code together, so that the C++ code can call the assembly code.
+// .global is important because it makes the functions available to the linker. Without it, the linker would not be able to find the functions in the assembly code.
+// extern "C" is important because it tells the compiler not to mangle the function names. Without it, the linker would not be able to find the functions in the assembly code.
+// depending on the compiler the function arguments are passed in different registers. The assembly code must be written to expect the arguments in the correct registers.
+// here the gcc compiler is used, which passes the first argument in r25 to r8.
 extern "C"
 {
   // function prototypes debug
@@ -57,29 +64,29 @@ void setup()
 //   delay(500);
 // }
 
-// void loop()
-// {
-//   //LED - ON
-//   STRIP_show(LED_NUM_MAX - 1, 0, 0, 10, 5);
-//   STRIP_show(0, 0, 0, 10, 5);
-//   delay(20);
-//   //LED - OFF
-//   STRIP_show(LED_NUM_MAX, 0, 0, 0, 0);
-//   delay(20);
-// }
-
 void loop()
 {
-    END_frame(0); // 1 x End-Frame
-    delay(200);
-    END_frame(16); // 1 x End-Frame
-    delay(200);
-    END_frame(17); // 2 x End-Frame
-    delay(200);
-    END_frame(33); // 3 x End-Frame
-    delay(200);
-    END_frame(241); // 16 x End-Frame
-    delay(200);
-    END_frame(256); // 16 x End-Frame
-    delay(200);
+  //LED - ON
+  STRIP_show(LED_NUM_MAX - 1, 0, 0, 10, 5);
+  STRIP_show(0, 0, 0, 10, 5);
+  delay(20);
+  //LED - OFF
+  STRIP_show(LED_NUM_MAX, 0, 0, 0, 0);
+  delay(20);
 }
+
+// void loop()
+// {
+//     END_frame(0); // 1 x End-Frame
+//     delay(200);
+//     END_frame(16); // 1 x End-Frame
+//     delay(200);
+//     END_frame(17); // 2 x End-Frame
+//     delay(200);
+//     END_frame(33); // 3 x End-Frame
+//     delay(200);
+//     END_frame(241); // 16 x End-Frame
+//     delay(200);
+//     END_frame(256); // 16 x End-Frame
+//     delay(200);
+// }
